5e76dec087faf0eca44c949906c3a03c1d6cb4bb
Julian Diaz <diazjulian460@gmail.com>
2024-02-15 18:42:26 -0500

    Creación del proyecto

---
+++ src/main/kotlin/co/edu/uco/subscriptionprocessor/SubscriptionProcessorApplication.kt
@@ -0,0 +1,11 @@
+package co.edu.uco.subscriptionprocessor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscriptionProcessorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubscriptionProcessorApplication>(*args)
+}

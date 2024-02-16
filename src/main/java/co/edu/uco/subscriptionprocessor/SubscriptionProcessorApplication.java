a5ed91f8d9e97dc9d4fb2c65a88f12487fba011a
Julian Diaz <diazjulian460@gmail.com>
2024-02-15 19:04:33 -0500

    Se cambia de Kotlin a Java

---
+++ src/main/java/co/edu/uco/subscriptionprocessor/SubscriptionProcessorApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.subscriptionprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/subscriptionprocessor/SubscriptionProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.subscriptionprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
